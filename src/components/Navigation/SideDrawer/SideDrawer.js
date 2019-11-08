edfd8abc7e8e5b3b579b070dabc3a3f19bb62c20
Akanksha Sharma <akankshasharma2415@gmail.com>
2019-11-07 22:00:46 -0800

    added sidedrawer and sidedrawer-toggle button

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,29 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
             <nav>
                 <NavigationItems />
             </nav>
-        </div>
+            </div>
+        </Aux>
     );
-
 };
 
 export default sideDrawer;
