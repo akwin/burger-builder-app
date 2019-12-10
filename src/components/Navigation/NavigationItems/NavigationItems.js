30bd8d46929b1c62d4fe5992e2d800d42c03a032
Akanksha Sharma <akankshasharma2415@gmail.com>
2019-12-10 14:30:21 -0800

    added authentication

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,10 +2,15 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' exact> Burger Builder </NavigationItem>
-        <NavigationItem link='/orders'> My Orders </NavigationItem>
+        { props.isAuthenticated 
+            ? <NavigationItem link='/orders'> My Orders </NavigationItem>
+            : null}
+        { !props.isAuthenticated 
+            ? <NavigationItem link='/auth'>Authenticate</NavigationItem>
+            : <NavigationItem link='/logout'>Logout</NavigationItem>}
     </ul>
 );
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from '../../axios-orders';
+
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-    
-    componentDidMount() {
-        this.props.onFetchOrders();  //only want to fetch orders when it is mounted
+    componentDidMount () {
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render () {
         let orders = <Spinner />;
-        if (!this.props.loading) {
-            orders = this.props.orders.map(order => (
-                <Order 
+        if ( !this.props.loading ) {
+            orders = this.props.orders.map( order => (
+                <Order
                     key={order.id}
                     ingredients={order.ingredients}
-                    price={order.price}/>
-            ))
+                    price={order.price} />
+            ) )
         }
         return (
             <div>
@@ -33,15 +33,16 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token, userId) => dispatch( actions.fetchOrders(token, userId) )
     };
 };
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( Orders, axios ) );
