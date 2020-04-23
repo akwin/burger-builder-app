44ea05c932e4ab7198fc54f9d2f17f0a8c644be1
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-23 11:53:23 -0700

    update price label

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,7 +16,7 @@ const OrderSummary = props => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger"
                     clicked={props.purchaseCancelled}>CANCEL</Button>
